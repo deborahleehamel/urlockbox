fbf12d837cdbcc13448ca7c1e3a5ed2861a20cb5
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-31 16:04:39 -0600

    user cannot log in with invalid email or password

--- spec/features/user_can_login_and_logout_spec.rb
+++ spec/features/user_can_login_and_logout_spec.rb
@@ -13,5 +13,29 @@ RSpec.feature "User can Log in and Log out" do
       expect(current_path).to eq links_path
       expect(page).to have_content user.email
     end
+
+    scenario "user unable to log in with invalid email" do
+      visit login_path
+
+      fill_in "Email", with: "Big fat greek wedding"
+      fill_in "Password", with: "password"
+      click_button "Log in"
+
+      expect(page).to have_content("Oops! Please try again.")
+      expect(page).to have_content("Log in")
+    end
+
+    scenario "user with account cannot log in with invalid password" do
+      user = create(:user, email: "deb@tnemail.com", password: "password")
+      visit login_path
+
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "badpassword"
+      click_button "Log in"
+
+      expect(page).to have_content("Oops! Please try again.")
+      expect(page).to have_content("Log in")
+    end
+
   end
 end
