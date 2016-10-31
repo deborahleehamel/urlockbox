6087e9e4d4397102eedf119fa90f763368d0c6fe
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-31 16:07:08 -0600

    user sees log out after logging in and log in after logging out add log out contest to user_can_login_and_logout_spec

--- spec/features/user_can_login_and_logout_spec.rb
+++ spec/features/user_can_login_and_logout_spec.rb
@@ -36,6 +36,23 @@ RSpec.feature "User can Log in and Log out" do
       expect(page).to have_content("Oops! Please try again.")
       expect(page).to have_content("Log in")
     end
+  end
 
+  context "see Log out" do
+    scenario "user gets redirected to log in page after log out" do
+      user = create(:user, email: "deb@tnemail.com", password: "password")
+      visit login_path
+
+      fill_in "Email", with: user.email
+      fill_in "Password", with: "password"
+      click_button "Log in"
+
+      expect(page).to have_content(user.email)
+
+      click_link "Log out"
+
+      expect(page).to have_content("You are logged out.")
+      expect(page).to have_content("Log in")
+    end
   end
 end
