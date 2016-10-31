d8cd2aa57a4fc2e42f2ab2ef1e0a67915f0ff684
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-31 14:16:05 -0600

    add home_controller make root the home#show route add links to home page

--- spec/features/guest_visits_homepage_spec.rb
+++ spec/features/guest_visits_homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Guest visits '/'" do
-  scenario "see buttons to log in or sign up" do
+  scenario "see linls to log in or sign up" do
     visit '/'
     expect(page).to have_link('Log In')
     expect(page).to have_link('Sign Up')
