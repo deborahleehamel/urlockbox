00507fd169274ebc8fba446c4561d636bcb2ac25
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-31 21:05:55 -0600

    update links index and links controller user_can_add_links_spec passing see current user's links under All My Links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,24 @@
 class LinksController < ApplicationController
+  before_action :require_user
 
   def index
+    @links = current_user.links
   end
+
+  def create
+    @link = current_user.links.new(link_params)
+    if @link.save
+      flash[:success] = 'New link has been added!'
+      redirect_to links_path
+    else
+      flash.new[:error] = "Link is not valid. Please try adding again."
+      redirect_to links_path
+    end
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:title, :url, :id)
+    end
 end
