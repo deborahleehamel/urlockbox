f0826f5fea7226a18799e00c7766152a8a01b927
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-31 18:16:37 -0600

    add has_many :links to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :links
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, on: create
   validates :password_digest, presence: true
