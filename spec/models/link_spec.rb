4e96db276f558e515feaa8def9f89db79a49cbbd
Deborah Hamel <deborahleehamel@gmail.com>
2016-11-08 14:50:18 -0700

    add test for read default on link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Link, type: :model do
     link = Link.new(title: "Love", url: "lovefest")
     expect(link.valid?).to be false
   end
+
+  it "should have false as default on read" do
+    link = Link.create(title: "Winner", url: "http://hilary.com", user_id: 1 )
+
+    expect(link.read).to eq false
+  end
 end
