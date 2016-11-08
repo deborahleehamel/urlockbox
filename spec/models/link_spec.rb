ae2d45cc64fcdab1e1b0bb4d84719e63478fb3ee
Deborah Hamel <deborahleehamel@gmail.com>
2016-11-08 11:06:22 -0700

    add model test to link

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Link, type: :model do
   it { should validate_presence_of(:url) }
   it { should validate_presence_of(:title) }
   it { should belong_to(:user) }
+
+  it "should check if url valid" do
+    link = Link.new(title: "Love", url: "lovefest")
+    expect(link.valid?).to be false
+  end
 end
