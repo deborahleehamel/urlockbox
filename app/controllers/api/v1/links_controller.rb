ca3ad4531e55337b09ce63d2b10de860a924e77b
Deborah Hamel <deborahleehamel@gmail.com>
2016-11-08 19:26:17 -0700

    remove strikethrough and still working on status update

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,15 +1,12 @@
 class Api::V1::LinksController < ApplicationController
 
-  def index
-    render json: Link.where(user: current_user)
-  end
-
   def update
     link = Link.find[params[:id]]
     if params[:read]
       link.read = !link.read
       link.save
-      render json: link
+    else
+      link.update(params)      
     end
   end
 
