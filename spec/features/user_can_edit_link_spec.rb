de6de568f3a3577c0eb8551d6d835e713740e5c1
Deborah Hamel <deborahleehamel@gmail.com>
2016-11-08 22:24:03 -0700

    add a sad path to edit spec

--- spec/features/user_can_edit_link_spec.rb
+++ spec/features/user_can_edit_link_spec.rb
@@ -18,4 +18,23 @@ RSpec.feature "User can edit a link" do
       expect(page).to_not have_content "http://www.ted.com"
     end
   end
+
+  context "url is not valid" do
+    scenario "message appears on page" do
+      user = User.create(email: "deb@tnemail.com", password: "password")
+      login(user)
+
+      fill_in "Title", with: "TED: Ideas worth spreading"
+      fill_in "Url", with: "https://www.ted.com/"
+      click_on "Add new link"
+
+      click_on "Edit"
+
+      fill_in "Url", with: "bubbles"
+      click_on "Update Link"
+      expect(page).to_not have_content "bubbles"
+    end
+  end
+
+
 end
